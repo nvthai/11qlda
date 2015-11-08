e754b6a1aa912071462a332634f50097599b8356
Lê Hoàn Hảo <lehoanhao.94@gmail.com>
2015-11-07 23:32:26 -0800

    [Back-end] Đính kèm file

---
+++ Sourcecode/Hao_Dinh kem file/process.php
@@ -0,0 +1,21 @@
+﻿<?php
+$file_name=$_FILES['upload']['name'];
+$extent_file="gif|jpg|png|docx";
+$pattern='#.+\.(gif|jpg|png|docx)$#i';
+if(preg_match($pattern,$file_name)==1){
+	$file_type=true;
+}else{
+	$file_type=false;
+}
+if($file_type==true){
+	$source=$_FILES['upload']['tmp_name'];
+	$dest='images/'.$_FILES['upload']['name'];
+	if(copy($source,$dest)==true){
+	$flag=true;
+	echo 'Upload thành công';
+	}else{
+	$flag=false;
+	echo 'Upload thất bại';
+	}
+}
+?>
---
+++ Sourcecode/Hao_Dinh kem file/upload.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Đính kèm file</title>
+</head>
+<body>
+<h1>Đính kèm file</h1>
+<form id="myform" name="myform" method="post" enctype="multipart/form-data" action="process.php">
+	<input type="file" name="upload" id="upload"></input>
+	<br>
+	<input type="submit" name="botton" id="botton" value="submit"></input>
+</form>
+</body>
+</html>
