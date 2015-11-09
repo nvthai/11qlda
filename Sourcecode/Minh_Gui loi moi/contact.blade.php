a95110c2d784bfc2de22013b559c5e8a404a7d60
pnminh205 <pnminh205@gmail.com>
2015-11-10 01:03:32 +0700

    Gui loi moi

---
+++ Sourcecode/Minh_Gui loi moi/contact.blade.php
@@ -0,0 +1,44 @@
+@extends('layout.default')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Gửi lời mời</div>
+                <div class="panel-body">
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    <form class="form-horizontal" role="form" method="POST" action="{!! url('contact') !!}">
+                        {!! csrf_field() !!}
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Nhập Địa Chỉ Email</label>
+                            <div class="col-md-6">
+                                <input type="email" class="required" id="email" name="email" value="">
+                            </div>
+                        </div>
+
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Gửi lời mời
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
