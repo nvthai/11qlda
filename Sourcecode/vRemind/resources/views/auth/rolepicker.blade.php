df31f26b703ecc10edbb87b3cec115e6247e07f0
Thai Nguyen Van <1152003@student.hcmus.edu.vn>
2015-11-22 17:33:56 +0700

    Ráp code front-end, còn lỗi không hiển thị được hình ảnh trong button

--- Sourcecode/vRemind/resources/views/auth/rolepicker.blade.php
+++ Sourcecode/vRemind/resources/views/auth/rolepicker.blade.php
@@ -3,20 +3,59 @@
 	Select role
 @endsection
 @section('content')
-	{!! "user role: " .Auth::user()->role !!}
-
-	{!! Form::open(array('route' => 'role.selected')) !!}
-		{!! Form::hidden('role', 'teacher') !!}
-		{!! Form::submit('Teacher', array('class' => 'btn')) !!}
-	{!! Form::close() !!}
-
-	{!! Form::open(array('route'=> 'role.selected')) !!}
-		{!! Form::hidden('role', 'parent') !!}
-		{!! Form::submit('Parents', array('class' => 'btn')) !!}
-	{!! Form::close() !!}
-	
-	{!! Form::open(array('route' => 'role.selected')) !!}
-		{!! Form::hidden('role', 'student') !!}
-		{!! Form::submit('Student', array('class' => 'btn')) !!}
-	{!! Form::close() !!}
+    <div class="mot-hang" style="background-color:#FAFAF9;">
+        <div class="mot-hang-content" style="text-align:center;height:100%;">
+            <div class="mot-hang" style="margin-top: 125px;
+font-size: 20px; color: #5D5D5D;margin-bottom: 5px;">
+                Tell us about yourself
+            </div>
+            
+            <div class="khung-chua-chucvu">
+                <div class="khung-chua-icon">
+                    <img alt="image" src="../resources/assets/img/teacher.svg"/>
+                </div>
+                <div class="mot-hang" style="text-align:center;margin-top:15px;">
+                	{!! Form::open(array('route' => 'role.selected')) !!}
+						{!! Form::hidden('role', 'teacher') !!}
+						{!! Form::submit('I\'m a teacher', array('class' => 'btn')) !!}
+					{!! Form::close() !!}
+                </div>
+            </div>
+            
+            <div class="khung-chua-chucvu">
+                <div class="khung-chua-icon">
+                    <img alt="image" src="../resources/assets/img/student.svg"/>
+                </div>
+                <div class="mot-hang" style="text-align:center;margin-top:15px;">
+                	{!! Form::open(array('route'=> 'role.selected')) !!}
+						{!! Form::hidden('role', 'student') !!}
+						{!! Form::submit('I\'m a student', array('class' => 'btn', 'image' => '../resources/assets/img/student.svg')) !!}
+					{!! Form::close() !!}
+                </div>
+            </div>
+            
+            <div class="khung-chua-chucvu">
+                <div class="khung-chua-icon">
+                    <img alt="image" src="../resources/assets/img/parent.svg"/>
+                </div>
+                <div class="mot-hang" style="text-align:center;margin-top:15px;">
+					{!! Form::open(array('route' => 'role.selected')) !!}
+						{!! Form::hidden('role', 'parent') !!}
+						{!! Form::submit('I\'m a parent', array('class' => 'btn')) !!}
+					{!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <script>
+        $(".khung-chua-chucvu").mouseover(function(){
+           $(this).css("background-color","#ECECEC");
+           $(this).find(".khung-chua-icon").css("background-color","white"); 
+        });
+        $(".khung-chua-chucvu").mouseout(function(){
+           $(this).css("background-color","white");
+           $(this).find(".khung-chua-icon").css("background-color","#fafaf9"); 
+        });
+    </script>
 @endsection
