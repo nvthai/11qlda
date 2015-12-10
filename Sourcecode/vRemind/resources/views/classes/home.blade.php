3b1995bc9fefa18b1dac671ee2d56230b19b5415
ttthanh <trantrungthanh910@gmail.com>
2015-12-10 11:27:34 +0700

    Noi dung dang notification

--- Sourcecode/vRemind/resources/views/classes/home.blade.php
+++ Sourcecode/vRemind/resources/views/classes/home.blade.php
@@ -27,7 +27,7 @@
             </div>
         </div>
         
-        <div class="main-area" style="margin-top: 90px;">
+        <div class="mot-hang" >
             <div class="panel-body">
                 {{-- You are logged in! --}}
                 {{-- $user = auth(); --}}
@@ -41,14 +41,14 @@
             </div>
                 
             <form role="form" method="POST" enctype="multipart/form-data" action="{{ url('/classes/upload') }}">
-              <div class="form-group">
+              <div class="form-group" style="margin-bottom:0px">
                 <div class="input-group">
                     <span class="input-group-addon">To: </span>
                     <input type="text" class="form-control" id="toClass" name="toClass" placeholder="{!!Session::get('sesClassId')->class_name!!}" disabled>
                 </div>
               </div>
-              <div class="form-group">
-                <textarea class="form-control" name="content" rows="3" placeholder="Type your annoucement to {!!Session::get('sesClassId')->class_name!!}"></textarea>
+              <div class="form-group" style="margin:-4px 0px 15px 0px;">
+                <textarea style="padding-top:8px;" class="form-control" name="content" rows="3" placeholder="Type your annoucement to {!!Session::get('sesClassId')->class_name!!}"></textarea>
               </div>
               <div class="btn-group" role="group" aria-label="...">
                 <input type="file" name="file" class="btn btn-default">
@@ -58,35 +58,210 @@
               <button  type="submit" class="btn btn-primary">Send</button>            
             </form>
             
-    <!--Can chinh sua giao dien o day-->
-        
-          
+    
+            <style>
+            .khung-chua-noi-dung-notifi
+            {
+                margin-top: 20px;
+                padding:20px 3% 0px 5%;
+                border:1px solid #E2E2E2;
+                border-radius:5px;
+            }
+            .ten-khung-notify{
+                font-weight:bold;
+                font-size: 16px;
+                font-family: monospace;
+                color:black;
+            }
+            .khung-chua-tweet-sendag{
+              display:none;
+              color: #4a89dc;
+              font-weight: bold;
+              padding-top: 10px;
+            }
+            .noi-dung-chua-tweet-send{
+              cursor:pointer;
+              opacity:1;
+
+            }           
+            .noi-dung-chua-tweet-send:hover{
+              opacity:0.7;
+            }
+
+            .content-count-icon-have{
+              float: left;
+              padding: 4px 10px 4px 25px;
+              background-image: url("../resources/assets/img/count-icon-notifi.png");
+              background-repeat: no-repeat;
+              background-size: 200px;
+              font-weight: bold;
+
+            }
+            .content-icon-notification
+            {
+              float: right;
+              position:relative;
+              width:70px;
+              height:70px;
+              background-image: url("../resources/assets/img/icon-content-notifi.png");
+              background-repeat: no-repeat;
+              background-size: 200px;
+              font-weight: bold;
+            }
+            .khung-chon-icon-notifi{
+                display:none;
+                  position: absolute;
+                  left: -65px;
+                  top: 85%;
+                  padding: 20px;
+                  border-radius: 20px;
+                  background-color: rgba(0, 0, 0, 0.78);
+            }
+                
+            </style>
+            <?php
+                $soluongngoissao = 1;
+                $soluongcheck = 2;
+                $soluongdelete = 4;
+                $soluongquestion = 3;
+                //ngoisao--check--delete--question
+                $noidungUserChon = "check";
+            ?>
             @foreach($notifications as $noti)                        
               @if($noti->class_id == Session::get('sesClassId')->class_id)
-              <div class = "panel panel-default" style="margin-top: 20px;">
-                <div style="padding-left: 15px; padding-top: 15px;">
-                  <?php 
-                    $sender = vRemind\User::find($noti->sender_id); 
-                    $name = $sender->name;
-                  ?>
-                <b> {{$name}}</b>
+              <div class = "mot-hang khung-chua-noi-dung-notifi">
+                <div class="mot-hang">
+                  <div class="mot-hang-70">
+                    <div class="mot-hang ten-khung-notify">
+                      <?php 
+                        $sender = vRemind\User::find($noti->sender_id); 
+                        $name = $sender->name;
+                      ?>
+                      {{$name}}
+                    </div>
+                    <div class="mot-hang" style="color:#CCCCCC;">
+                      {{$noti->created_at}}
+                    </div>
+                  </div>
+                  <div class="mot-hang-30 khung-chua-tweet-sendag">
+                    <span class="mot-hang-50 noi-dung-chua-tweet-send" >
+                        Tweet
+                    </span>
+                    <span class="mot-hang-50 noi-dung-chua-tweet-send" >
+                        Send again
+                    </span>
+                  </div>
                 </div>
-                <div style="padding-left: 15px;">
-                  {{$noti->created_at}}
-                </div>
-                <div class = "panel-body">
+
+                <div class ="mot-hang" style="margin:20px 0px 15px 0px;">
                   {{$noti->content}}
                 </div>
+
+                <div class="mot-hang">
+                  <div class="mot-hang-70" style="padding-top:12px;">
+                        <?php
+                        if($soluongngoissao > 0)
+                        {
+                        ?>
+                        <span class="content-count-icon-have">
+                          {{$soluongngoissao}}
+                        </span>
+
+                        <?php
+                        }
+                        if($soluongcheck > 0)
+                        {
+                        ?>
+                        <span class="content-count-icon-have" style="background-position:-52px 0px;">
+                          {{$soluongcheck}}
+                        </span>
+                        <?php
+                        }
+                        if($soluongdelete > 0)
+                        {
+                        ?>
+                        <span class="content-count-icon-have" style="background-position:-113px 1px;">
+                          {{$soluongdelete}}
+                        </span>
+                        <?php
+                        }
+                        if($soluongquestion > 0)
+                        {
+                        ?>
+                        <span class="content-count-icon-have" style="background-position:-172px 1px;">
+                          {{$soluongquestion}};
+                        </span>
+                        <?php
+                        }
+                        ?>
+                  </div>
+                  <div class="mot-hang-30">
+                    <div class="content-icon-notification" style=
+                        <?php 
+                        if($noidungUserChon == "ngoisao")
+                        {
+                          echo '"background-position:0px -71px;"';
+                        }
+                        if($noidungUserChon == "check")
+                        {
+                         echo '"background-position:0px -148px;"' ;
+                        }
+                        if($noidungUserChon == "delete")
+                        {
+                          echo '"background-position:0px -228px;"';
+                        }
+                        if($noidungUserChon == "question")
+                        {
+                          echo '"background-position:0px 0px;"';
+                        }
+                        ?> onclick="MoKhungChonIcon({{$noti->class_id}})">
+
+                        <div class="khung-chon-icon-notifi" id=<?php echo '"khung-chua-icon-id'.$noti->class_id.'"' ?>>
+                          <span class="icon-noitifi-child">
+                          </span>
+                          <span class="icon-noitifi-child">
+                          </span>
+                          <span class="icon-noitifi-child">
+                          </span>
+                          <span class="icon-noitifi-child">
+                          </span>
+                        </div>
+                    </div>
+
+
+                  </div>
+                  
+                </div>
+
                 @if($noti->file != null)
                 <div class = "panel-body">
                   <img src="{!! $noti->file !!}"   style="width:304px;height:228px;">
                 </div>
-
                 @endif
+
+
+
+
               </div>
                @endif          
             @endforeach
-          
+          <script>
+            function MoKhungChonIcon(giaTri)
+            {
+              var idKhungChua = "#khung-chua-icon-id" + giaTri;
+              $(idKhungChua).css("display","block");
+
+              setTimeout(function(){
+                $(idKhungChua).css("display","none");
+              },3000);
+
+            }
+            $(".khung-chua-noi-dung-notifi").hover(function(){
+              $(this).find(".khung-chua-tweet-sendag").css("display","block");
+            },function(){
+              $(this).find(".khung-chua-tweet-sendag").css("display","none");
+            });
+          </script>
 
 
 
